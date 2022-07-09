3686f4b171837cc185516032c2477a18f027b96b
itariss <itariss@protonmail.com>
2022-07-09 03:08:53 -0300

    conditions based on screen width

--- js/carousel.js
+++ js/carousel.js
@@ -1,12 +1,27 @@
 let bannerBg = document.querySelector(".banner__bg");
 let imageNum = "1";
 
+let screenWidth = window.innerWidth;
+
+console.log(screenWidth);
+
 function setNewImage() {
 	imageNum++;
 	if (imageNum > 3) {
 		imageNum = 1;
 	}
-	bannerBg.style.background = `url(../../img/carousel-${imageNum}.jpg)70% / cover`;
+
+	if (screenWidth <= 767) {
+		bannerBg.style.background = `url(../../img/carousel-${imageNum}p.png)center / cover`;
+	}
+
+	if (screenWidth > 767) {
+		bannerBg.style.background = `url(../../img/carousel-${imageNum}m.png)center / cover`;
+	}
+
+	if (screenWidth >= 1440) {
+		bannerBg.style.background = `url(../../img/carousel-${imageNum}.png)center / cover`;
+	}
 }
 
 setInterval(setNewImage, 3500);
