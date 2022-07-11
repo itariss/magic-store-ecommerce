1a1ab9e2e336d75599dd7147d661cabce2ab96d9
itariss <itariss@protonmail.com>
2022-07-11 13:12:50 -0300

    script for description page

--- js/descricao.js
+++ js/descricao.js
@@ -2,7 +2,7 @@ let produtos = document.querySelectorAll(".produtos__img");
 
 produtos.forEach(imagem => {
 	imagem.addEventListener("click", () => {
-		window.location.href = "/./descricao.html";
+		window.location.href = "./html/descricao.html";
 		window.sessionStorage.setItem("imagem", JSON.stringify(imagem.src));
 	});
 });
