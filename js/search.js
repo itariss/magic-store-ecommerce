446d7f5055aaf06f474d2d40efa654f6550c83dc
itariss <itariss@protonmail.com>
2022-07-26 13:29:17 -0300

    search script

---
+++ js/search.js
@@ -0,0 +1,63 @@
+import { recebeProdutos } from "./criaProduto-box.js";
+
+const searchInput = document.querySelector("[data-menu-search]");
+
+searchInput.addEventListener("input", () => {
+	let searchValue = new RegExp(`${searchInput.value}`, "gi");
+
+	console.log(searchValue);
+
+	if (searchInput.value === "") {
+		return displayDefault();
+	}
+
+	recebeProdutos().then(data => {
+		data.forEach(produto => {
+			if (searchValue.test(produto.nome)) {
+				showBox(produto.id);
+			} else if (!searchValue.test(produto.nome)) {
+				hideBox(produto.id);
+			}
+
+			toggleSection();
+		});
+	});
+});
+
+const toggleSection = () => {
+	let sessaoCategoria = document.querySelectorAll(".produtos");
+	sessaoCategoria.forEach(sessao => {
+		if (sessaoCategoria.innerHTML === "") {
+			sessao.classList.add("none");
+		} else {
+			sessao.classList.remove("none");
+		}
+	});
+};
+
+const displayDefault = () => {
+	const todosProdutos = document.querySelectorAll(".produtos__box");
+	todosProdutos.forEach(caixa => {
+		caixa.classList.remove("none");
+	});
+};
+
+const hideBox = dadoId => {
+	console.log("era para esconder" + dadoId);
+	const todosProdutos = document.querySelectorAll(".produtos__box");
+	todosProdutos.forEach(caixa => {
+		if (dadoId == caixa.id) {
+			caixa.classList.add("none");
+		}
+	});
+};
+
+const showBox = dadoId => {
+	console.log(dadoId);
+	const todosProdutos = document.querySelectorAll(".produtos__box");
+	todosProdutos.forEach(caixa => {
+		if (dadoId == caixa.id) {
+			caixa.classList.remove("none");
+		}
+	});
+};
